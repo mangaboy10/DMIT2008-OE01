8779bd5dc44ce83ff0f144b75e29751f8e3d9f11
Beni-Axel Ntwari <nikayuta8@gmail.com>
2026-05-14 18:55:58 -0600

    second commit, first one didnt work because i forgot the git add . but imported data, grabbed dom elements, created function to render out data, created the function for form submission and added event listener to expense form

--- 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
+++ 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
@@ -1,1 +1,50 @@
 //Instruction On expense-tracker-code-explained.md
+//Instruction On expense-tracker-code-explained.md
+
+import theExpenses from "./expense-data";
+
+// 2. Grab the element DOM elements
+const expenseContainer = document.getElementById("expense-container");
+
+const searchItem = document.getElementById('searchbox');
+const expenseForm = document.getElementById('expense-form-add'); 
+
+//3. render out data into a grid of cards
+function renderExpenses(expenseData){
+    console.log(expenseData)
+    // clear out existing 
+
+    expenseContainer.innerHTML = "";
+
+    expenseData.forEach(
+        // for a given expense,
+        (expense) => {
+            expenseContainer.innerHTML += `
+             <div class="card" id="${expense.id}">
+              <div class="header">
+                <div>
+                  <div class="title">${expense.title}</div>
+                  <div class="meta category">${expense.category}</div>
+                </div>
+                <div class="amount">${expense.amount}</div>
+              </div>
+              <div class="meta date">${expense.date}</div>
+              <div class="actions">
+                <button class="edit-btn" id="${expense.id}">Edit</button>
+                <button class="delete-btn" id="${expense.id}">Delete</button>
+              </div>
+            </div>
+            `
+        }
+    )
+}
+
+// 4. call the function to actually do the render
+renderExpenses(theExpenses);
+
+
+5. // form submission
+function submitExpense() {
+    
+}
+
